bb3102981c296c7915aad3bbcf119e7b8bb840dd
Aizaz Khan <Aizaz Khan@DESKTOP-RFN0FAF>
2024-11-26 17:21:30 +0500

    Add project files.

---
+++ GAME.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Student {
+public:
+	string name;
+	string city;
+	int age;
+
+
+	void getinput() {
+		cout << "ENTER YOUR NAME:";
+		getline(cin, name);
+		cout << "ENTER YOUR CITY NAME:";
+		getline(cin, city);
+		cout << "ENTER YOUR AGE:";
+		cin >> age;
+		cout << endl;
+
+	}
+	
+
+};
+
+int main() {
+
+	Student s1;
+	s1.getinput();
+	s1.print();
+
+}
